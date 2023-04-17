372713be8164e6a66aba1d80b640eb692ea2bd15
Aaron Yee <aaronyee97@gmail.com>
2023-04-18 03:25:03 +0800

    Revert "feat(estimateUserOp): add callGasLimit when estimate"
    This reverts commit 8c8a6eb2d99db7e140c519cc35f0803a3c6713cb.

--- packages/bundler/src/UserOpMethodHandler.ts
+++ packages/bundler/src/UserOpMethodHandler.ts
@@ -108,8 +108,7 @@ export class UserOpMethodHandler {
       maxFeePerGas: 0,
       maxPriorityFeePerGas: 0,
       preVerificationGas: 0,
-      verificationGasLimit: 10e6,
-      callGasLimit: 10e6
+      verificationGasLimit: 10e6
     }
 
     // todo: checks the existence of parameters, but since we hexlify the inputs, it fails to validate
